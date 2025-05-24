aa078906ccc66514fc7488a6a813010d131defce
Jamie York <ziadoz@gmail.com>
2025-05-24 21:56:11 +0100

    output something

--- index.php
+++ index.php
@@ -9,4 +9,6 @@ $html  = new HtmlDownloader(URL)->download();
 $items = new HtmlParser(URL, $html)->parse();
 $rss   = new RssFeed(URL, $items)->render();
 
-file_put_contents(__DIR__ . '/output/apple-trailers.rss', $rss);
+file_put_contents($output = __DIR__ . '/output/apple-trailers.rss', $rss);
+
+echo 'Generated RSS Feed: ' . $output . PHP_EOL;
